9434305ca7ca6206cfc802eb2daae164b7abb9e0
DavidSpartan95 <davidulwhan@hotmail.com>
2022-12-06 15:33:50 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, Button, Modal, Pressable, TextInput,ActivityIndicator,ImageBackground } from 'react-native';
+import { Atricle,LoadMore } from "./Components"
+import { useState } from 'react';
+
 
 export default function App() {
+  const [model, setModel] = useState(false)
+  const [userName, setUserName] = useState("Guest")
+  const [isLoading, setIsLoading] = useState(false)
+  const [userPic, setUserPic] = useState(require("./assets/icon.png"))
+  let tempName = ""
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+
+        <Text style={{ color: "white", fontSize: 31, marginLeft: 10 }}>Hello {userName}!</Text>
+        <Pressable
+          onPress={() => {
+            setModel((prevValue)=> !prevValue)
+            }}>
+          
+          <ImageBackground style={{ height: 70, width: 70, marginRight: 10,  }}
+            imageStyle = {{borderRadius:200}}
+            source={userPic}
+          >
+            <Text style={{backgroundColor:"white",fontWeight:"bold",textAlign:"center", marginTop:50}}>{userName}</Text>
+          </ImageBackground>
+          
+        </Pressable>
+      </View>
+
+      <Text style={{ marginTop: 10, fontWeight: "bold", fontSize: 23, marginLeft: "10%" }}>Today's Highlights</Text>
+
+      <ScrollView style={{ marginLeft: "10%", marginRight: "10%" }}>
+
+        <Atricle title={"First Article"} />
+        <Atricle title={"Another Article"} />
+        <Atricle title={"Important One"} />
+        <ActivityIndicator
+           animating={isLoading}
+            size="small"
+            color="#0000ff"
+          />  
+        <LoadMore setIsLoading={setIsLoading}/>
+
+      </ScrollView>
+
+      <Modal visible={model} transparent={true} animationType="fade">
+        
+        <View style={{ flex: 1,backgroundColor: 'rgba(52, 52, 52, 0.6)',justifyContent: "center",}}>
+          <View style={{height:150,width:250,backgroundColor:"white", alignSelf:"center",borderRadius: 5,}}>
+            <Pressable
+            onPress={() => {
+            setModel((prevValue)=> !prevValue)
+          }}>
+              <Image style={{ height: 25, width: 25,}}
+              source={require("./assets/close.png")}
+              />
+            </Pressable>
+            <Text style={{fontWeight:"bold", fontSize:20,marginLeft:20}}>Name:</Text>
+            <TextInput
+                placeholder="Initial Text"
+                onChangeText={(value)=>{
+                  tempName = value
+                }}  
+                
+              />
+            <Button title='Submit'
+              
+              onPress={() =>{
+                setUserPic(require("./assets/User.png"))
+                setUserName(tempName)
+                setModel((prevValue)=> !prevValue)}
+                }
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
+
+
   );
 }
 
@@ -14,7 +88,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+
+  },
+  header: {
+    marginTop: 30,
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    height: 100,
+    width: "100%",
+    backgroundColor: "#632c8a",
+    flexDirection: "row",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
   },
 });
